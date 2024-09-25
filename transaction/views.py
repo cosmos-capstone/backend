359cf508feaf3fe740ee225f686f2e0bb9d24653
Youngho Cho <dominoyh5@gmail.com>
2024-09-25 21:27:20 +0900

    update: return db contents

--- transaction/views.py
+++ transaction/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from django.core import serializers
+from .models import Transaction
 
 def dumpdata(request):
-    return JsonResponse({'message': 'Hello, World!'})
+    return JsonResponse({'message': 'success', 'data': list(Transaction.objects.values())})
