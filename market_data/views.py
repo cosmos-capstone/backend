fd567a65dd64ca61fe902071c6e291907ab0e19e
Youngho Cho <dominoyh5@gmail.com>
2024-10-25 19:39:47 +0900

    add: yfinance

--- market_data/views.py
+++ market_data/views.py
@@ -1,10 +1,16 @@
 from django.http import JsonResponse
 from django.db import connections
 from django.views import View
+import yfinance as yf
 
 class Stock(View):
     def get(self, request, *args, **kwargs):
         symbol = request.GET.get('symbol')
-        market_data_connection = connections['market_data']
 
-        return JsonResponse({"message": symbol})
+        stock = yf.Ticker("005930.KS")
+
+        market_data_connection = connections['market_data']
+        history = stock.history(period="max")
+
+        history_list = history.values.tolist()
+        return JsonResponse({"message": history_list})
