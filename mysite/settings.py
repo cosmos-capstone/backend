13a5d8bd7d1373bca43c44d6fdcefa0499a722c6
Youngho Cho <dominoyh5@gmail.com>
2024-10-26 14:42:05 +0900

    update: settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'transaction',
-    'market_data',
+    'transaction.apps.TransactionConfig',
+    'market_data.apps.MarketDataConfig',
 ]
 
 MIDDLEWARE = [
