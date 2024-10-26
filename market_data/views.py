6ecf797a877ef17f64bb5539c22336758b763dbd
Youngho Cho <dominoyh5@gmail.com>
2024-10-26 16:00:18 +0900

    update: symbol query

--- market_data/views.py
+++ market_data/views.py
@@ -1,16 +1,12 @@
 from django.http import JsonResponse
-from django.db import connections
 from django.views import View
 import yfinance as yf
 
 class Stock(View):
     def get(self, request, *args, **kwargs):
         symbol = request.GET.get('symbol')
-
-        stock = yf.Ticker("005930.KS")
-
-        market_data_connection = connections['market_data']
+        stock = yf.Ticker(symbol)
         history = stock.history(period="max")
 
         history_list = history.values.tolist()
-        return JsonResponse({"message": history_list})
+        return JsonResponse({"name": stock.info['longName'], "message": history_list})
