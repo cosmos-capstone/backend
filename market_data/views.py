3850f1583b538e0206856f615b2344c2ab887751
Youngho Cho <dominoyh5@gmail.com>
2024-10-26 17:38:02 +0900

    update: history period to max

--- market_data/views.py
+++ market_data/views.py
@@ -9,7 +9,7 @@ class Stock(View):
     def get(self, request, *args, **kwargs):
         symbol = request.GET.get('symbol')
         stock = yf.Ticker(symbol)
-        history = stock.history(period="5d")
+        history = stock.history(period="max")
         currency = stock.info['currency']
 
         for datetime, close_price in history['Close'].items():
@@ -19,7 +19,7 @@ class Stock(View):
                 close_price=close_price,
                 currency=currency
             )
-            stock_price.save()
+            # stock_price.save()
 
         history_list = history.values.tolist()
         return JsonResponse({"name": stock.info['longName'], "message": history_list})
