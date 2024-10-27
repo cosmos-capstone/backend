2b7c2d1ccd01e62a8412f9251762e6f77bf5b59f
Youngho Cho <dominoyh5@gmail.com>
2024-10-27 16:09:27 +0900

    add: api summary to Stock

--- market_data/views.py
+++ market_data/views.py
@@ -1,9 +1,23 @@
+from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from django.http import JsonResponse
-from django.views import View
 
 from .models import StockPrice
 
-class Stock(View):
+class Stock(APIView):
+    @extend_schema(
+        summary="Get Stock Prices",
+        description="Fetches stock prices for a given symbol, including name, currency, and historical data.",
+        parameters=[
+            OpenApiParameter(
+                name="symbol",
+                type=str,
+                location=OpenApiParameter.QUERY,
+                required=True,
+                description="The stock symbol for which to retrieve prices (e.g., 'AAPL' for Apple Inc. and '005930.KS' for Samsung Electorinics)"
+            )
+        ],
+    )
     def get(self, request, *args, **kwargs):
         symbol = request.GET.get('symbol')
         name, currency, data = StockPrice.get_stock_prices(symbol)
