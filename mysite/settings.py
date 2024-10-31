b30e1846f8852e386bd0795e70a37d5f3bc4b4ba
Youngho Cho <dominoyh5@gmail.com>
2024-10-31 20:12:10 +0900

    add: cors settigns

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'transaction.apps.TransactionConfig',
     'market_data.apps.MarketDataConfig',
     'drf_spectacular',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +134,9 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://cosmos-capstone.github.io',
+]
