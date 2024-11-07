345272c3710573d676fabf05035e27d8bd1fd5e1
Youngho Cho <dominoyh5@gmail.com>
2024-11-07 14:23:20 +0900

    add: path for rebalancing in trasaction

--- transaction/urls.py
+++ transaction/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('dumpdata1', views.dumpdata1, name='dumpdata1'),
     path('test', views.TransactionView.as_view(), name='test'),
     path('myaccount', views.TransactionView.as_view(), name='test'),
+    path('rebalancing', views.TransactionView.as_view(), name='rebalancing'),
 ]
